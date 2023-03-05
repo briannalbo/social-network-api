02711beac31595f539c0e04ea18f46e71211efd8
unknown <brianna.albo@gmail.com>
2023-03-05 18:37:20 -0500

    adds user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      min_length: 6,
+      max_length: 25,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+    ]
+}
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
