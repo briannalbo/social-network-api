054ab2ac6f7d0f27a0c9b4a75edd315b3d8f0a11
unknown <brianna.albo@gmail.com>
2023-03-05 20:23:26 -0500

    added thought model and updated user model with virtuals

--- models/User.js
+++ models/User.js
@@ -26,10 +26,19 @@ const userSchema = new Schema(
       {
         type: Schema.Types.ObjectId,
         ref: 'User',
-      }
-    ]
+      },
+    ],
+},
+{    toJSON: {
+  virtuals: true,
+},
+id: false,
 }
 );
+
+userSchema.virtual('friendCount').get(function () {
+return this.friends.length;
+});
 
 const User = model('User', userSchema);
 
