a16fe7155ae90f9a6fe7e793939359cc0fd07efd
unknown <brianna.albo@gmail.com>
2023-03-09 16:18:22 -0500

    added function for creating a reaction & added routing for reactions

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { getThoughts, createThought } = require('../../controllers/thought-controller');
+const { getThoughts, createThought, createReaction } = require('../../controllers/thought-controller');
 
 router.route('/').get(getThoughts).post(createThought);
+router.route('/:thoughtId/reactions').post(createReaction);
 
 module.exports = router;
